16d6b093773deb7bd41224401116a296d6b3a8e4
vinaygupta88 <142647437+vinaygupta88@users.noreply.github.com>
2025-04-15 09:43:07 +0530

    model
    Model (rfp.pickle) ,  and other changes in views.py etc

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,3 +1,22 @@
+import os
+import pickle
+import numpy as np
 from django.shortcuts import render
 
-# Create your views here.
+model_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'rfp.pkl')
+model = pickle.load(open(model_path, 'rb'))
+
+def index(request):
+    return render(request, 'weather_app/index.html')
+
+def about(request):
+    return render(request, 'weather_app/about.html')
+
+def predict(request):
+    return render(request, 'weather_app/predict.html')
+
+def submit(request):
+    if request.method == "POST":
+        
+        return render(request, 'weather_app/result.html', {'result': result})
+    return render(request, 'weather_app/predict.html')
