3aee62d106362e232bc55194c71099ca5c0c8710
vinaygupta88 <142647437+vinaygupta88@users.noreply.github.com>
2025-04-19 17:01:09 +0530

    Update settings.py
    static path for image, css is updated

--- weather_project/weather_project/settings.py
+++ weather_project/weather_project/settings.py
@@ -1,6 +1,6 @@
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -106,7 +106,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR / 'weather_app/static' ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'weather_app/static'),
+]
+TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'weather_app/templates')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
